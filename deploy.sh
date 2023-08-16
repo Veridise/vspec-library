e20be3b98a8371505da2dedc060f6c5134f81b52
Nikos Chondros <nikos@veridise.com>
2023-08-16 20:51:42 +0300

    Added deploy script, assumes aws client is properly configured

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+	echo "Must specify one of 'dev', 'internal' as a target for the deployment of the V spec library"
+	exit 1
+fi
+LATEST_FILE="vspec_library.latest"
+TARGET="$1"
+VERSION=$(cat $LATEST_FILE)
+VERSION_FILE=""vspec_library_$VERSION.json""
+
+read -p "Press ENTER to deploy $VERSION_FILE and $LATEST_FILE to environment $TARGET"
+
+case "$TARGET" in
+	dev | internal)
+		echo "Uploading version file $VERSION_FILE to s3://veridise-$TARGET/vspeclib/"
+		aws s3 cp "$VERSION_FILE" "s3://veridise-$TARGET/vspeclib/"
+		echo "Uploading $LATEST_FILE to s3://veridise-$TARGET/vspeclib/"
+		aws s3 cp "$LATEST_FILE" s3://veridise-$TARGET/vspeclib/
+		;;
+	*)
+		echo Unknown target environment: $TARGET
+		exit 1
+esac
+echo "Done"
