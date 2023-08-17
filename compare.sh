0e8a2e28c98bb43620f4a24dea3a7f996785c196
Nikos Chondros <nikos@veridise.com>
2023-08-17 09:47:24 +0300

    Fix typo

--- compare.sh
+++ compare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 read -p "Warning: This will delete the broken-down library and recreate it from the yaml file. Hit Ctrl+Break to stop if this is not what you expect!"
-rm -rm library
+rm -rf library
 ./breakdown.py
 ./compile.py
 cat vspec_library_old.json | sed 's/"category": 0/"category": "erc20"/g' | sed 's/"category": 1/"category": "erc721"/g' | sed 's/"category": 2/"category": "erc3156"/g' | jq > nx.old
